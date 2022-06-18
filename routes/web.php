177758f0db7e8e3d4f7ab0c8e9c025a60f19004c
catsound25 <catsound25@gmail.com>
2022-06-19 02:22:57 +0900

    デバッグ用環境変数追加、コメント追記、ルーティングを仮で追記

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TopController::class, 'index']);
+// // portfolio top
+// Route::get('/', [TopController::class, 'index']);
+// // search music
+// Route::get('/', [TopController::class, 'index']);
+// // search user
+// Route::get('/', [TopController::class, 'index']);
+// // user detail
+// Route::get('/', [TopController::class, 'index']);
+// // simple login
+// Route::get('/', [TopController::class, 'index']);
+// // add favorite
+// Route::get('/', [TopController::class, 'index']);
+// // del favorite
+// Route::get('/', [TopController::class, 'index']);
